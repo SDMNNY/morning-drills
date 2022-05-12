72868638b70f048c7cbd366eef6983198061d2f8
sdmnny <sdmnny@gmail.com>
2022-05-12 10:13:22 -0400

    created inital html and script file

---
+++ assests/script.js
@@ -0,0 +1,16 @@
+//pre class drill 5/12/2022
+
+ //Pre-Class Drill =
+ //Good morning! Here's a little exercise to get your JS juices flowing:
+ //Write a function that takes in a string and outputs the number of vowels (not counting y).
+ 
+ //Ex:
+ //Input: "hello"
+ //Output: 2
+ 
+ //Input: "you are great!"
+ //Output: 6
+ //
+ //Input: "why?"
+ //Output: 0
+ 
